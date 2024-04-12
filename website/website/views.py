ab62e3f47ff62a8f19f61910afee1811e39aa3d6
Szymon Piątek <szymon.piatek125@gmail.com>
2024-04-12 19:01:29 +0200

    Fix view's blocks ordering

--- website/website/views.py
+++ website/website/views.py
@@ -2,19 +2,20 @@ from django.shortcuts import render
 
 
 def main_panel(request):
-    blocks = {
-        "Panel administratora": {"Statystyki"},
-        "Rezerwacje": {"Pokój", "Biurko", "Sala", "Dodaj pozycję"},
-        "Przedmioty": {"Sprawdź stan", "Przeprowadź inwentaryzację", "Dodaj pozycję"},
-        "Pracownicy": {"Wyświetl wszystkich pracowników",
-                       "Wyświetl wszystkie grupy",
-                       "Dodaj pracownika",
-                       "Dodaj grupę"},
-        "Lokalizacje": {"Podgląd budynku", "Podgląd lokalizacji", "Dodaj lokalizację"},
-        "Kalendarz": {"Zobacz najbliższe wydarzenia", "Dodaj wydarzenie"}
-    }
+    blocks = [
+        ("Panel administratora", ["Statystyki"]),
+        ("Rezerwacje", ["Pokój", "Biurko", "Sala", "Dodaj pozycję"]),
+        ("Przedmioty", ["Sprawdź stan", "Przeprowadź inwentaryzację", "Dodaj pozycję"]),
+        ("Pracownicy", ["Wyświetl wszystkich pracowników",
+                        "Wyświetl wszystkie grupy",
+                        "Dodaj pracownika",
+                        "Dodaj grupę"]),
+        ("Lokalizacje", ["Podgląd budynku", "Podgląd lokalizacji", "Dodaj lokalizację"]),
+        ("Kalendarz", ["Zobacz najbliższe wydarzenia", "Dodaj wydarzenie"])
+    ]
 
     context = {
         "blocks": blocks,
     }
+
     return render(request, "main_panel.html", context)
