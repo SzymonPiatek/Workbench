35cfca3bf3d9d3003ecb82b68c05d87176dc6258
Szymon Piątek <119783254+SzymonPiatek@users.noreply.github.com>
2024-04-12 21:19:18 +0200

    Main panel scss and dict page builder (#2)
    * Main panel scss
    
    * Update view and scss
    
    * Poppins font
    
    * Home scss
    
    * Fix view's blocks ordering
    
    * func for hide elements
    
    * Block icons
    
    * Update view's blocks
    
    * Update main panel scss
    
    * Readme, dict page builder
    
    * Fix main panel scss

---
+++ readme.md
@@ -0,0 +1,3 @@
+## Main panel
+![img.png](website/documentation/main_panel.png)
+![img.png](website/documentation/main_panel_2.png)
--- website/website/views.py
+++ website/website/views.py
@@ -2,18 +2,67 @@ from django.shortcuts import render
 
 
 def main_panel(request):
-    blocks = {
-        "Panel administratora": {"Statystyki"},
-        "Rezerwacje": {"Pokój", "Biurko", "Sala"},
-        "Przedmioty": {"Sprawdź stan", "Przeprowadź inwentaryzację"},
-        "Pracownicy": {"Wyświetl wszystkich pracowników",
-                       "Wyświetl wszystkie grupy"},
-        "Lokalizacje": {"Podgląd budynku", "Podgląd lokalizacji"},
-        "Kalendarz": {"Zobacz najbliższe wydarzenia", "Dodaj wydarzenie"}
-    }
+    blocks = [
+        {
+            "title": "Panel administratora",
+            "elements": [
+                {"icon": "fa-chart-pie", "name": "Statystyki"},
+                {"icon": "fa-bell", "name": "Powiadomienia"},
+            ]
+        },
+        {
+            "title": "Rezerwacje",
+            "elements": [
+                {"icon": "fa-car", "name": "Parking"},
+                {"icon": "fa-display", "name": "Biurko"},
+                {"icon": "fa-people-roof", "name": "Sala"},
+                {"icon": "fa-plus", "name": "Dodaj pozycję"}
+            ]
+        },
+        {
+            "title": "Przedmioty",
+            "elements": [
+                {"icon": "fa-list", "name": "Lista przedmiotów"},
+                {"icon": "fa-magnifying-glass", "name": "Inwentaryzacja"},
+                {"icon": "fa-plus", "name": "Dodaj pozycję"}
+            ]
+        },
+        {
+            "title": "Pracownicy",
+            "elements": [
+                {"icon": "fa-user", "name": "Lista pracowników"},
+                {"icon": "fa-users", "name": "Lista grup"},
+                {"icon": "fa-user-plus", "name": "Dodaj pracownika"},
+                {"icon": "fa-plus", "name": "Dodaj grupę"}
+            ]
+        },
+        {
+            "title": "Lokalizacje",
+            "elements": [
+                {"icon": "fa-building", "name": "Podgląd budynku"},
+                {"icon": "fa-door-closed", "name": "Podgląd pomieszczeń"},
+                {"icon": "fa-plus", "name": "Dodaj pomieszczenie"}
+            ]
+        },
+        {
+            "title": "Kalendarz",
+            "elements": [
+                {"icon": "fa-calendar-check", "name": "Najbliższe wydarzenia"},
+                {"icon": "fa-plus", "name": "Dodaj wydarzenie"}
+            ]
+        },
+        {
+            "title": "Helpdesk",
+            "elements": [
+                {"icon": "fa-life-ring", "name": "Zgłoś problem"},
+                {"icon": "fa-eye", "name": "Zobacz zgłoszenia"},
+                {"icon": "fa-question-circle", "name": "FAQ"}
+            ]
+        }
+    ]
 
     context = {
-        "page_title": "Strona główna",
         "blocks": blocks,
     }
+
     return render(request, "main_panel.html", context)
