7c9ce4fe47fc17e9fecccb3ae259187594b3777b
Szymon Piątek <szymon.piatek125@gmail.com>
2024-04-14 18:09:33 +0200

    Change dict block attribute name

--- website/website/views.py
+++ website/website/views.py
@@ -14,58 +14,58 @@ def main_panel(request):
         {
             "title": "Panel administratora",
             "elements": [
-                {"icon": "fa-chart-pie", "name": "Statystyki", "view_name": "statistics_page"},
-                {"icon": "fa-bell", "name": "Powiadomienia", "view_name": "notifications_page"},
-                {"icon": "fa-plus", "name": "Dodaj powiadomienie", "view_name": "notifications_page"},
+                {"icon": "fa-chart-pie", "name": "Statystyki", "url": "statistics_page"},
+                {"icon": "fa-bell", "name": "Powiadomienia", "url": "notifications_page"},
+                {"icon": "fa-plus", "name": "Dodaj powiadomienie", "url": "notifications_page"},
             ]
         },
         {
             "title": "Rezerwacje",
             "elements": [
-                {"icon": "fa-car", "name": "Parking", "view_name": "main_panel_page"},
-                {"icon": "fa-display", "name": "Biurko", "view_name": "main_panel_page"},
-                {"icon": "fa-people-roof", "name": "Sala", "view_name": "main_panel_page"},
-                {"icon": "fa-plus", "name": "Dodaj rezerwację", "view_name": "main_panel_page"}
+                {"icon": "fa-car", "name": "Parking", "url": "main_panel_page"},
+                {"icon": "fa-display", "name": "Biurko", "url": "main_panel_page"},
+                {"icon": "fa-people-roof", "name": "Sala", "url": "main_panel_page"},
+                {"icon": "fa-plus", "name": "Dodaj rezerwację", "url": "main_panel_page"}
             ]
         },
         {
             "title": "Przedmioty",
             "elements": [
-                {"icon": "fa-list", "name": "Lista przedmiotów", "view_name": "main_panel_page"},
-                {"icon": "fa-magnifying-glass", "name": "Inwentaryzacja", "view_name": "main_panel_page"},
-                {"icon": "fa-plus", "name": "Dodaj pozycję", "view_name": "main_panel_page"}
+                {"icon": "fa-list", "name": "Lista przedmiotów", "url": "main_panel_page"},
+                {"icon": "fa-magnifying-glass", "name": "Inwentaryzacja", "url": "main_panel_page"},
+                {"icon": "fa-plus", "name": "Dodaj pozycję", "url": "main_panel_page"}
             ]
         },
         {
             "title": "Pracownicy",
             "elements": [
-                {"icon": "fa-user", "name": "Lista pracowników", "view_name": "main_panel_page"},
-                {"icon": "fa-users", "name": "Lista grup", "view_name": "main_panel_page"},
-                {"icon": "fa-user-plus", "name": "Dodaj pracownika", "view_name": "main_panel_page"},
-                {"icon": "fa-plus", "name": "Dodaj grupę", "view_name": "main_panel_page"}
+                {"icon": "fa-user", "name": "Lista pracowników", "url": "main_panel_page"},
+                {"icon": "fa-users", "name": "Lista grup", "url": "main_panel_page"},
+                {"icon": "fa-user-plus", "name": "Dodaj pracownika", "url": "main_panel_page"},
+                {"icon": "fa-plus", "name": "Dodaj grupę", "url": "main_panel_page"}
             ]
         },
         {
             "title": "Lokalizacje",
             "elements": [
-                {"icon": "fa-building", "name": "Podgląd budynku", "view_name": "main_panel_page"},
-                {"icon": "fa-door-closed", "name": "Podgląd pomieszczeń", "view_name": "main_panel_page"},
-                {"icon": "fa-plus", "name": "Dodaj pomieszczenie", "view_name": "main_panel_page"}
+                {"icon": "fa-building", "name": "Podgląd budynku", "url": "main_panel_page"},
+                {"icon": "fa-door-closed", "name": "Podgląd pomieszczeń", "url": "main_panel_page"},
+                {"icon": "fa-plus", "name": "Dodaj pomieszczenie", "url": "main_panel_page"}
             ]
         },
         {
             "title": "Kalendarz",
             "elements": [
-                {"icon": "fa-calendar-check", "name": "Najbliższe wydarzenia", "view_name": "main_panel_page"},
-                {"icon": "fa-plus", "name": "Dodaj wydarzenie", "view_name": "main_panel_page"}
+                {"icon": "fa-calendar-check", "name": "Najbliższe wydarzenia", "url": "main_panel_page"},
+                {"icon": "fa-plus", "name": "Dodaj wydarzenie", "url": "main_panel_page"}
             ]
         },
         {
             "title": "Helpdesk",
             "elements": [
-                {"icon": "fa-life-ring", "name": "Zgłoś problem", "view_name": "main_panel_page"},
-                {"icon": "fa-eye", "name": "Zobacz zgłoszenia", "view_name": "main_panel_page"},
-                {"icon": "fa-question-circle", "name": "FAQ", "view_name": "main_panel_page"}
+                {"icon": "fa-life-ring", "name": "Zgłoś problem", "url": "main_panel_page"},
+                {"icon": "fa-eye", "name": "Zobacz zgłoszenia", "url": "main_panel_page"},
+                {"icon": "fa-question-circle", "name": "FAQ", "url": "main_panel_page"}
             ]
         }
     ]
