593b8b995fc90f0d6d3c110adaeb919b753fad7e
Szymon Piątek <szymon.piatek125@gmail.com>
2024-04-14 20:01:07 +0200

    User tip for form init

--- website/website/urls.py
+++ website/website/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from . import apis
 
 
 urlpatterns = [
@@ -8,4 +9,6 @@ urlpatterns = [
     path('', views.main_panel, name="main_panel_page"),
     path('statistics/', views.statistics, name="statistics_page"),
     path('notifications/', include("notifications.urls")),
+
+    path('get_all_users/', apis.get_all_users, name="get_all_users_api")
 ]
