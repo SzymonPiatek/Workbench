830f9f5ddb800623c688fb208d98ccc36314687d
Szymon Piątek <szymon.piatek@pisf.pl>
2024-04-15 12:02:45 +0200

    Randommer api, create_fake_user

--- website/website/apis.py
+++ website/website/apis.py
@@ -1,9 +1,9 @@
 from django.http import JsonResponse
-from django.contrib.auth.models import User
+from users.models import CustomUser
 
 
 def get_all_users(request):
     search_text = request.GET.get('search', '')
-    users = User.objects.filter(username__icontains=search_text)
+    users = CustomUser.objects.filter(username__icontains=search_text)
     user_list = [{'username': user.username} for user in users]
     return JsonResponse(user_list, safe=False)
