ca859a00ec91830deca56fea720ba6ddc29c7452
Szymon Piątek <szymon.piatek@pisf.pl>
2024-04-20 16:15:05 +0200

    Fix command

--- website/users/management/commands/test_create_users.py
+++ website/users/management/commands/test_create_users.py
@@ -4,16 +4,15 @@ from django.core.management import call_command
 
 class Command(BaseCommand):
     help = "Test create users"
-
     def handle(self, *args, **kwargs):
-        call_command("create_fake_users", 1)
-        call_command("create_fake_users", 2)
-        call_command("create_fake_users", 4)
-        call_command("create_fake_users", 8)
-        call_command("create_fake_users", 16)
-        call_command("create_fake_users", 32)
-        call_command("create_fake_users", 64)
-        call_command("create_fake_users", 128)
-        call_command("create_fake_users", 256)
-        call_command("create_fake_users", 512)
-        call_command("create_fake_users", 1024)
+        call_command("create_users", 1)
+        call_command("create_users", 2)
+        call_command("create_users", 4)
+        call_command("create_users", 8)
+        call_command("create_users", 16)
+        call_command("create_users", 32)
+        call_command("create_users", 64)
+        call_command("create_users", 128)
+        call_command("create_users", 256)
+        call_command("create_users", 512)
+        call_command("create_users", 1024)
