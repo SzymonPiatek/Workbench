6ab66f33722586993c966018e62dd8a97fce0266
Szymon Piątek <szymon.piatek@pisf.pl>
2024-04-20 08:50:35 +0200

    Create_fake_users 50=15.79sec

--- website/users/management/commands/create_fake_users.py
+++ website/users/management/commands/create_fake_users.py
@@ -21,31 +21,42 @@ class Command(BaseCommand):
         existing_usernames = set(CustomUser.objects.values_list('username', flat=True))
         existing_emails = set(CustomUser.objects.values_list('email', flat=True))
 
+        batch_size = 100  # Definiujemy rozmiar partii
+
         users_to_create = []
 
         while len(users_to_create) < quantity:
-            first_name = fake.first_name()
-            last_name = fake.last_name()
-            full_name = f"{first_name} {last_name}"
-            username = f"{first_name[0].lower()}{last_name.lower()}"
-            username = unidecode(username)
-            pre_email = f"{first_name.lower()}.{last_name.lower()}"
-            pre_email = unidecode(pre_email)
-            email = f"{pre_email}@gmail.com"
+            user_data = []
+            for _ in range(quantity - len(users_to_create)):
+                first_name = fake.first_name()
+                last_name = fake.last_name()
+                full_name = f"{first_name} {last_name}"
+                username = f"{first_name[0].lower()}{last_name.lower()}"
+                username = unidecode(username)
+                pre_email = f"{first_name.lower()}.{last_name.lower()}"
+                pre_email = unidecode(pre_email)
+                email = f"{pre_email}@gmail.com"
+                password = make_password("123")
 
-            if username not in existing_usernames and email not in existing_emails:
-                user = CustomUser(
-                    first_name=first_name,
-                    last_name=last_name,
-                    full_name=full_name,
-                    username=username,
-                    email=email,
-                    password=make_password("123")
-                )
-                users_to_create.append(user)
-                existing_usernames.add(username)
-                existing_emails.add(email)
+                if username not in existing_usernames and email not in existing_emails:
+                    user_data.append(CustomUser(
+                        first_name=first_name,
+                        last_name=last_name,
+                        full_name=full_name,
+                        username=username,
+                        email=email,
+                        password=password
+                    ))
+                    existing_usernames.add(username)
+                    existing_emails.add(email)
 
-        CustomUser.objects.bulk_create(users_to_create, ignore_conflicts=True)
+                if len(user_data) >= batch_size:
+                    users_to_create.extend(user_data)
+                    CustomUser.objects.bulk_create(user_data, ignore_conflicts=True)
+                    user_data = []
+
+            users_to_create.extend(user_data)
+            CustomUser.objects.bulk_create(user_data, ignore_conflicts=True)
+
         elapsed_time = time.time() - start_time
-        print(f"{quantity} users created successfully in {elapsed_time:.2f} seconds.")
+        print(f"{quantity} użytkowników utworzono pomyślnie w ciągu {elapsed_time:.2f} sekund.")
