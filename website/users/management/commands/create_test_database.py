2d411fa6d21d7e1f099dd0942aa2eb5ce66668db
Szymon Piątek <szymon.piatek@pisf.pl>
2024-05-07 13:00:49 +0200

    Update command

--- website/users/management/commands/create_test_database.py
+++ website/users/management/commands/create_test_database.py
@@ -12,11 +12,11 @@ class Command(BaseCommand):
         call_command("create_users", 200)
         call_command("create_groups")
         call_command("add_worker_group")
-        call_command("create_superuser")
-        call_command("add_superuser_to_groups")
         call_command("create_fake_addresses", 20)
         call_command("create_fake_rooms")
         call_command("create_fake_items")
+        call_command("create_superuser")
+        call_command("add_superuser_to_groups")
 
         elapsed_time = time.time() - start_time
         self.stdout.write(self.style.SUCCESS(f"Test database created in {elapsed_time:.2f} sec"))
